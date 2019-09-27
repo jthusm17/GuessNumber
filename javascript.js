2344c472ba61d968668cc23d63c63822e7975a44
jthusm17 <54552549+jthusm17@users.noreply.github.com>
2019-09-27 16:07:53 -0500

    Fixes and additions (from recomendations)

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+// initalize guessees
+let numGuesses = 1
+
 // random num
 let autoNum = Math.floor(Math.random() * 15 + 1);
 console.log("the number is " + autoNum);
@@ -5,27 +8,31 @@ console.log("the number is " + autoNum);
 //button stuff + input + restart
 const disable=() => {
    document.getElementById("guessField").disabled = true;
-   document.getElementById("myBtn2").disabled = true;
+   document.getElementById("submitBtn").disabled = true;
+   document.getElementById("spinner").hidden = false;
    console.log("did disable");
    }
 const enable=() => {
    document.getElementById("guessField").disabled = false;
-   document.getElementById("myBtn2").disabled = false;
+   document.getElementById("submitBtn").disabled = false;
+   document.getElementById("spinner").hidden = true;
    console.log("did enable");
    }
 const restart=() => {
-   Guess = 0;
+   guess = 0;
    autoNum = Math.floor(Math.random() * 15 + 1);
    numGuesses = 1;
-   document.getElementById("guess2").innerHTML = "Guess# 1";
-   document.getElementById("guess1").innerHTML = "...";
-   document.getElementById("myBtn1").disabled = true;
-   document.getElementById("myBtn2").disabled = false;
+   document.getElementById("guessCount").innerHTML = "Guess# 1";
+   document.getElementById("helpText").innerHTML = "...";
+   document.getElementById("restartBtn").disabled = true;
+   document.getElementById("submitBtn").disabled = false;
    document.getElementById("guessField").hidden = false;
-   document.getElementById("lbl").innerHTML = "Guess a Number";
-   document.getElementById("bottomT").innerHTML = "Input your guess into the text box above."; 
-   console.log("buttons/lbl/textfield should have changed");
-   console.log("the number is " + autoNum);
+   document.getElementById("restartBtn").hidden = true;
+   document.getElementById("submitBtn").hidden = false;
+   document.getElementById("sideLabel").innerHTML = "Guess a Number";
+   document.getElementById("bottomText").innerHTML = "Input your guess into the text box above."; 
+   console.log("buttons/lbl/textfield returned to origanal state");
+   console.log("the next number is " + autoNum);
 }
 
 // adds setTimout
@@ -33,42 +40,49 @@ const timePat=() => {
    disable();
     timeVar = setTimeout(pat, 3000);
 }
-// initalize guessees
-let numGuesses = 1
 
 // big boy function- controls the verything
-const pat=() => { 
-  
-// number guessed      
-let Guess = document.getElementById("guessField").value; 
-console.log(Guess);
-
+const pat=() => {  
+// number guessed + does the stuff    
+let guess = document.getElementById("guessField").value; 
+console.log(guess);
 enable();
-if(Guess == autoNum) 
+if(guess == autoNum) 
 {   
-document.getElementById("guess1").innerHTML = " Congrats, u guessed right in '" + numGuesses + "' guess(es)";
+document.getElementById("helpText").innerHTML = " Congrats, you guessed right in '" + numGuesses + "' guesses";
 console.log("congrats you got it right in " + numGuesses)
-document.getElementById("guess2").innerHTML = "Guess#: " + numGuesses;
-document.getElementById("myBtn1").disabled = false; 
+document.getElementById("guessCount").innerHTML = "Guess# " + numGuesses;
+document.getElementById("restartBtn").disabled = false; 
 document.getElementById("guessField").value = "";  
-document.getElementById("guessField").hidden = true; 
-document.getElementById("lbl").innerHTML = "Press 'Restart' to play again!";
-document.getElementById("bottomT").innerHTML = " ";
-document.getElementById("myBtn2").disabled = true;
+document.getElementById("guessField").hidden = true;
+document.getElementById("submitBtn").hidden = true; 
+document.getElementById("sideLabel").innerHTML = "Press 'Restart' to play again!";
+document.getElementById("bottomText").innerHTML = " ";
+document.getElementById("submitBtn").disabled = true;
+document.getElementById("restartBtn").hidden = false
+// grammer fix if guessess = 1
+if(numGuesses == 1 ) 
+{
+   document.getElementById("helpText").innerHTML = " Congrats, you guessed right in '" + numGuesses + "' guess";
+}
 } 
-else if(Guess > autoNum)  
+else if(guess > autoNum)  
 { 
 numGuesses++; 
-document.getElementById("guess1").innerHTML = " Try Smaller";
- console.log("try smaller")
- document.getElementById("guess2").innerHTML = "Guess#: " + numGuesses;
+document.getElementById("helpText").innerHTML = " Try Smaller";
+console.log("try smaller");
+document.getElementById("guessCount").innerHTML = "Guess# " + numGuesses;
 } 
-else
+else if(guess < autoNum)
 { 
 numGuesses++;
-document.getElementById("guess1").innerHTML = " Try Bigger";
-console.log("try bigger")
-document.getElementById("guess2").innerHTML = "Guess #: " + numGuesses;
-
+document.getElementById("helpText").innerHTML = " Try Bigger";
+console.log("try bigger");
+document.getElementById("guessCount").innerHTML = "Guess# " + numGuesses;
 } 
+else 
+{
+document.getElementById("helpText").innerHTML = "Invalid. Use integers please.";
+console.log("Invalid input = " + guess + ". Guess not counted");
+}
 } 
